83bb02a2cc9069b066acfa297ca1fc4ebf0d90eb
JYL <JYL@DESKTOP-TORHHDE>
2017-06-22 13:52:12 +0900

    gallery css 추가

---
+++ src/main/java/com/jx372/mysite/controller/GalleryController.java
@@ -0,0 +1,24 @@
+package com.jx372.mysite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/gallery")
+public class GalleryController {
+	
+	@RequestMapping("")
+	public String index(){
+		
+		return "gallery/index";
+	}
+	
+	/*public String upload(){
+		
+	}
+	
+	public String delete
+	*/
+	
+
+}
